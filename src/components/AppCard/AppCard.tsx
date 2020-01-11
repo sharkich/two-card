c9c60df06f8652ee0b62f18caf8497c30b88b6c0
Artem Koziar <koziar.artem@gmail.com>
2020-01-11 14:44:16 +0200

    Add Cards color

--- src/components/AppCard/AppCard.tsx
+++ src/components/AppCard/AppCard.tsx
@@ -1,14 +1,22 @@
 import React, { FunctionComponent } from 'react';
+import clsx from 'clsx';
 import Card from '../../interfaces/Card';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import { createStyles, Theme } from '@material-ui/core';
+import Suit from '../../interfaces/Suit';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     Card: {},
     CardSymbol: {
-      fontSize: '100pt',
-      textShadow: '2px 2px 2px red'
+      fontSize: '100pt'
+      // textShadow: '2px 2px 2px red'
+    },
+    Black: {
+      color: 'black'
+    },
+    Red: {
+      color: 'red'
     }
   })
 );
@@ -17,10 +25,19 @@ interface Props {
   card: Card;
 }
 
-const AppCard: FunctionComponent<Props> = ({ card }) => {
+const AppCard: FunctionComponent<Props> = ({ card }: Props) => {
   const classes = useStyles();
+
+  const isBlackSuit = (suit: Suit) => [Suit.Spades, Suit.Clubs].includes(suit);
+  const isRedSuit = (suit: Suit) => [Suit.Hearts, Suit.Diamonds].includes(suit);
+
   return (
-    <div className={classes.Card}>
+    <div
+      className={clsx(classes.Card, {
+        [classes.Black]: isBlackSuit(card.suit),
+        [classes.Red]: isRedSuit(card.suit)
+      })}
+    >
       <span className={classes.CardSymbol}>{card.symbol}</span>
     </div>
   );
