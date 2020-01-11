3e435f0bf2021a822dfa5936c58e0295406bbddb
Artem Koziar <koziar.artem@gmail.com>
2020-01-11 21:34:40 +0200

    Add getWinner tests

--- src/models/Game.test.ts
+++ src/models/Game.test.ts
@@ -37,3 +37,47 @@ describe('getPairs', () => {
     ]);
   });
 });
+
+describe('getWinner', () => {
+  test('Should return correct with empty data', () => {
+    expect(game.getWinner([])).toEqual([]);
+  });
+
+  test('Should return correct with first win', () => {
+    expect(game.getWinner([{ pairs: [1] } as any, { pairs: [] } as any])).toEqual([0]);
+  });
+
+  test('Should return correct with second win', () => {
+    expect(game.getWinner([{ pairs: [1] } as any, { pairs: [1, 2] } as any, { pairs: [1] } as any])).toEqual([1]);
+  });
+
+  test('Should return correct with third win', () => {
+    expect(game.getWinner([{ pairs: [1] } as any, { pairs: [1, 2] } as any, { pairs: [1, 3, 4] } as any])).toEqual([2]);
+  });
+
+  test('Should return correct with first and second win', () => {
+    expect(game.getWinner([{ pairs: [1, 2] } as any, { pairs: [1, 2] } as any, { pairs: [1] } as any])).toEqual([0, 1]);
+  });
+
+  test('Should return correct with first and third win', () => {
+    expect(
+      game.getWinner([
+        { pairs: [1, 2, 3] } as any,
+        { pairs: [1, 2] } as any,
+        { pairs: [1, 3, 4] } as any,
+        { pairs: [1] } as any
+      ])
+    ).toEqual([0, 2]);
+  });
+
+  test('Should return correct with all', () => {
+    expect(
+      game.getWinner([
+        { pairs: [1, 2, 3] } as any,
+        { pairs: [1, 2, 4] } as any,
+        { pairs: [1, 3, 4] } as any,
+        { pairs: [5, 6, 7] } as any
+      ])
+    ).toEqual([0, 1, 2, 3]);
+  });
+});
